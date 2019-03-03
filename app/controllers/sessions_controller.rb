427bd1bce3118ae48c0e1128f7d3c582fc696c9b
github username <github email address>
2019-03-03 10:46:43 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    session[:name] = params[:name]
+      if session[:name] == nil || session[:name] == ""
+        redirect_to '/sessions/new'
+      else
+        redirect_to '/'
+      end
+
+   end
+
+
+  def destroy
+    session.delete :name
+    redirect_to '/sessions/new'
+  end
+end
